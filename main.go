e151c3c624d6a5b50c047aa90f3b6b1348346e4a
cg33 <chg80333@163.com>
2020-04-13 23:58:54 +0800

    update to v1.2.8

--- main.go
+++ main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/GoAdminGroup/go-admin/context"
-	"github.com/GoAdminGroup/go-admin/template/types/action"
 	"log"
 	"net/http"
 	"os"
@@ -11,16 +10,22 @@ import (
 
 	ada "github.com/GoAdminGroup/go-admin/adapter/gin"
 	_ "github.com/GoAdminGroup/go-admin/modules/db/drivers/mysql"
+	_ "github.com/GoAdminGroup/themes/adminlte"
 	_ "github.com/GoAdminGroup/themes/sword"
 
 	"github.com/GoAdminGroup/components/echarts"
 	"github.com/GoAdminGroup/demo_en/login"
 	"github.com/GoAdminGroup/demo_en/pages"
 	"github.com/GoAdminGroup/demo_en/tables"
+	"github.com/GoAdminGroup/filemanager"
+	"github.com/GoAdminGroup/go-admin/context"
 	"github.com/GoAdminGroup/go-admin/engine"
 	"github.com/GoAdminGroup/go-admin/modules/config"
+	"github.com/GoAdminGroup/go-admin/plugins/admin/modules/form"
 	"github.com/GoAdminGroup/go-admin/template"
 	"github.com/GoAdminGroup/go-admin/template/chartjs"
+	"github.com/GoAdminGroup/go-admin/template/types"
+	"github.com/GoAdminGroup/go-admin/template/types/action"
 	"github.com/gin-gonic/gin"
 )
 
@@ -39,6 +44,14 @@ func main() {
 	cfg := config.ReadFromJson(rootPath + "/config.json")
 	cfg.CustomFootHtml = template.HTML(`<div style="display:none;">
     <script type="text/javascript" src="https://s9.cnzz.com/z_stat.php?id=1278156902&web_id=1278156902"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-103003647-2"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+	gtag('config', 'UA-103003647-2');
+	</script>
 </div>`)
 	cfg.CustomHeadHtml = template.HTML(`<link rel="icon" type="image/png" sizes="32x32" href="//quick.go-admin.cn/official/assets/imgs/icons.ico/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="96x96" href="//quick.go-admin.cn/official/assets/imgs/icons.ico/favicon-64x64.png">
@@ -48,10 +61,31 @@ func main() {
 		Type:     "fadeInUp",
 		Duration: 0.9,
 	}
+	//cfg.HideConfigCenterEntrance = true
+	cfg.AddUpdateProcessFn(func(values form.Values) (values2 form.Values, e error) {
+		if values.Get("theme") == "adminlte" && values.Get("asset_url") == "//quick.go-admin.cn/demo/sword" {
+			values.Add("asset_url", "//quick.go-admin.cn/demo")
+		}
+		if values.Get("theme") == "sword" && values.Get("asset_url") == "//quick.go-admin.cn/demo" {
+			values.Add("asset_url", "//quick.go-admin.cn/demo/sword")
+		}
+		if values.Get("site_off") == "true" || values.Get("no_limit_login_ip") == "false" {
+			return nil, errors.New("permission denied")
+		}
+		return values, nil
+	})
 
 	if err := eng.AddConfig(cfg).
 		AddGenerators(tables.Generators).
 		AddGenerator("user", tables.GetUserTable).
+		AddPlugins(filemanager.NewFileManagerWithConfig(filemanager.Config{
+			Path:          "/data/www/go-admin-en/fm_example",
+			AllowDelete:   false,
+			AllowUpload:   true,
+			AllowRename:   true,
+			AllowDownload: true,
+			AllowMove:     true,
+		})).
 		AddNavButtons("Website Info", "", action.PopUp("/website/info", "Website Info",
 			func(ctx *context.Context) (success bool, msg string, data interface{}) {
 				return true, "ok", `<p>Created by <a href="https://github.com/chenhg5">cg33<a/></p>`
@@ -65,7 +99,13 @@ func main() {
 
 	// you can custom your pages like:
 
-	r.GET("/admin", ada.Content(pages.GetDashBoard2Content))
+	r.GET("/admin", ada.Content(func(ctx *gin.Context) (panel types.Panel, e error) {
+		if config.GetTheme() == "adminlte" {
+			return pages.GetDashBoardContent(ctx)
+		} else {
+			return pages.GetDashBoard2Content(ctx)
+		}
+	}))
 	r.GET("/admin/echarts", ada.Content(pages.GetDashBoard3Content))
 	r.GET("/admin/form1", ada.Content(pages.GetForm1Content))
 	r.GET("/admin/table", ada.Content(pages.GetTableContent))
